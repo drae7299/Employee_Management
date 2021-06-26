b155894404c08ba46639c275e5bb04d81fd28809
Andre Rodriguez <drae7299@gmail.com>
2021-06-26 01:36:35 -0400

    added in starter code to seed js

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -24,9 +24,39 @@ CREATE TABLE employee (
   PRIMARY KEY (id),
   FOREIGN KEY (role_id) REFERENCES role(id)
 );
-INSERT INTO department(name)
+INSERT INTO
+  department(name)
 VALUES
-("Sales"),
-("Engineering"),
-("Finance"),
-("Legal");
+  ("Sales"),
+  ("Engineering"),
+  ("Finance"),
+  ("Legal");
+INSERT INTO
+  role (title, salary, department_id)
+VALUES
+  ("Sales Lead", 70000.00, 001),
+  ("Salesperson", 50000.00, 001),
+  ("Lead Engineer", 110000.00, 002),
+  ("Software Engineer", 95000.00, 002),
+  ("Account Manager", 65000.00, 001),
+  ("Accountant", 100000.00, 003),
+  ("Legal Team Lead", 90000.00, 004);
+INSERT INTO
+  employee(first_name, last_name, role_id, manager_id)
+VALUES
+  ("Lynette", "Price", 1, NULL),
+  ("Hugh", "Bailey", 2, 1),
+  ("Dolores", "Schneider", 2, 1),
+  ("Jeanette", "Watkins", 3, NULL),
+  ("Mike", "Web", 4, 4),
+  ("Eileen", "Jensen", 4, 4),
+  ("Lela", "Freeman", 4, 4),
+  ("Oliver", "Higgins", 7, NULL),
+  ("Casey", "Cooper", 5, 1),
+  ("Alan", "Zimmerman", 5, 1),
+  ("Kim", "McCormick", 6, NULL),
+  ("Antonio", "Page", 4, 4),
+  ("Hannah", "Porter", 2, 1),
+  ("Bradley", "McLaughlin", 6, NULL),
+  ("Nancy", "Cobb", 7, NULL),
+  ("Seth", "Long", 6, NULL);
